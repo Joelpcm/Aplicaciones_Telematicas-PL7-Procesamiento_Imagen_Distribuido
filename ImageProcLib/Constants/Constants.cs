eb2fd370a5359df8872c46f8557c8df8964c1fad
Joel Pérez Camacho <UO289552@uniovi.es>
2025-04-25 19:01:21 +0200

    Creada una clase en la libreria que guardara constantes. Se ha cambiado la versiond e RabbitMQ a la ultima de la 6 (se actualizo a la 7 automaticamente). Se ha trabajado en el Productor de iamgenes, se probara cuandos e tenga el visualizador.

---
+++ ImageProcLib/Constants/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcLib.Constants
+{
+    public class Constants
+    {
+        public const string RabbitMQ_Server_IP = "192.168.0.27"; // IP del servidor RabbitMQ (maquina Ubuntu en este caso)
+    }
+}
--- Productor_imagenes/Program.cs
+++ Productor_imagenes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using RabbitMQ.Client;
 using ImageProcLib.Vocabulary;
+using ImageProcLib.Constants;
 using System;
 using System.Text;
 
@@ -9,7 +10,28 @@ namespace Productor_imagenes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string EXCHANGE = "ImageExchange";
+            const string ROUTING_KEY = "Image.Raw";
+
+            var factory = new ConnectionFactory() { HostName = ImageProcLib.Constants.Constants.RabbitMQ_Server_IP };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(EXCHANGE, "topic");
+
+                    var random = new Random();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        string payload = $"Image_{random.Next(1, 100)}";
+                        var message = MessageVocabulary.CreateMessage(i, ROUTING_KEY, payload);
+                        var body = MessageVocabulary.EncodeMessage(message);
+
+                        channel.BasicPublish(EXCHANGE, ROUTING_KEY, null, body);
+                        Console.WriteLine($"[Productor] Enviado: {payload}");
+                    }
+                }
+            }
         }
     }
 }
