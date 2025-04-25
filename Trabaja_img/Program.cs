09975631ef48e75c6e09b1d562594dc1ecd60378
Joel Pérez Camacho <UO289552@uniovi.es>
2025-04-25 18:02:29 +0200

    Add project files.

---
+++ Trabaja_img/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Trabaja_img
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
