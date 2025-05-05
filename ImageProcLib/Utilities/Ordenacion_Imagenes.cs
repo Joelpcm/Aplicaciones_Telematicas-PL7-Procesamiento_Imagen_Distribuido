690444c276b772dbd40bfd0e9f68512d76b52a44
uo289552 <uo289552@uniovi.es>
2025-05-05 17:21:35 +0200

    Añadidos comentarios.

--- ImageProcLib/Utilities/Ordenacion_Imagenes.cs
+++ ImageProcLib/Utilities/Ordenacion_Imagenes.cs
@@ -10,11 +10,15 @@ namespace ImageProcLib.Utilities
         public required string Type { get; set; } 
     }
 
+    // Clase encargada de gestionar y ordenar imágenes en base a su ID.
     public class Image_Sorter
     {
+        // Almacena las imágenes en un diccionario donde la clave es el ID de la imagen
         private readonly ConcurrentDictionary <int, ImageData> _imageBuffer = new();
+        // ID esperado de la siguiente imagen
         private int _expectedId = 0;
 
+        // Agrega una nueva imagen a la lista de imágenes
         public void AddImage(int id, byte[] image, DateTime timestamp, string type)
         {
             var imageData = new ImageData
@@ -24,14 +28,21 @@ namespace ImageProcLib.Utilities
                 Timestamp = timestamp,
                 Type = type
             };
+
+            // Almacena la imagen utilizando su ID como clave.
             _imageBuffer[id] = imageData;
         }
 
+        // Obtiene la siguiente imagen en orden según el ID esperado
         public ImageData? GetNextImage()
         {
+            // Intenta obtener la imagen correspondiente al ID esperado.
             if (_imageBuffer.TryRemove(_expectedId, out var imageData))
             {
+                // Si se encuentra la imagen, se incrementa el ID esperado para la siguiente imagen.
                 _expectedId++;
+
+
                 return imageData;
             }
             else
